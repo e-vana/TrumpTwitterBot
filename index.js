1e5e78a278649b64439bd88f0f6b528f829dffdb
Evan Vana <evjava@gmail.com>
2018-08-07 09:37:55 -0400

    added post to twitter functionality

--- index.js
+++ index.js
@@ -69,25 +69,26 @@ function fetchPosts() {
 }
 
 // Check this function over and be ready to delete a tweet.
-function addPost(tweetText, replyTo) {
+function addPost(tweetText, imgId) {
   // DOCS FOR THIS ENDPOINT
   // https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
 
   var client = new Twitter({
     consumer_key: process.env.API_KEY,
     consumer_secret: process.env.API_SECRET_KEY,
-    access_token_key: '',
-    access_token_secret: ''
+    access_token_key: process.env.API_ACCESS_TOKEN,
+    access_token_secret: process.env.API_ACCESS_TOKEN_SECRET
   });
 
   var params = {
     status: tweetText,
-    in_reply_to_status_id: replyTo
+    // in_reply_to_status_id: replyTo,
+    media_ids: imgId
 
   };
 
   return new Promise(function (resolve, reject) {
-    client.get('statuses/update', params, function (error, tweet, response) {
+    client.post('statuses/update', params, function (error, tweet, response) {
       if (!error) {
         resolve(tweet);
       }
@@ -221,9 +222,17 @@ async function testLoop() {
   // console.log(encodedData);
 
   var twitterImgUploadResponse = await uploadImage(encodedData);
-  console.log(twitterImgUploadResponse)
+  var idStr = JSON.parse(twitterImgUploadResponse);
+  var idStrValue = idStr.media_id_string;
+
+  var postText = 'MOCKING TRUMP' + ' ' + '@z1mb0bw4y';
+
+  var post = await addPost(postText, idStrValue);
+  console.log(post);
 }
 
+
+// function addPost(tweetText, replyTo, imgId) {
 testLoop();
 
 

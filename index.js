fd6583d7d613d1c8cff427a5dfa22882edd6fbe7
Evan Vana <evjava@gmail.com>
2018-08-09 15:36:05 -0400

    FIREBASE REFACTOR
    Giant refactor using firebase firestorage, need to clean up old functions

--- index.js
+++ index.js
@@ -38,10 +38,33 @@ const addPost = require("./functions/addPost.js");
 const fetchMemeUrl = require("./functions/fetchMemeUrl.js");
 const imgToBase = require("./functions/imgToBase.js");
 
-//Test Flag
-
 // Required to use .env
 dotenv.config();
+
+// Firebase Configuration
+const firebase = require('firebase/app');
+require('firebase/firestore')
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+}
+firestore.settings(settings);
+
+
+var db = firebase.firestore();
+
+
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -51,23 +74,91 @@ app.listen(PORT, () => {
   console.log("Server started on " + PORT);
 });
 
+
+function readDb() {
+  var theseTweets = [];
+  return new Promise(function (resolve, reject) {
+    db.collection("Tweets").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        theseTweets.push(doc.data());
+        // resolve(doc.data());
+        resolve(theseTweets);
+      })
+    })
+  })
+}
+
+// function readDb() {
+//   var theseTweets = [];
+//   return new Promise(function (resolve, reject) {
+//     db.collection("Tweets").get().then((querySnapshot) => {
+//       console.log(querySnapshot.docs[0].data());
+//     }
+//     ).catch(function (error) {
+//       console.log(error);
+//     })
+//   })
+// }
+
+
+
+
+// readDb();
+
+
+function writeDb(Tweet_ID, Tweet_Text) {
+  return new Promise(function (resolve, reject) {
+    db.collection("Tweets").add({
+      id: Tweet_ID,
+      text: Tweet_Text,
+    })
+      .then(function (docRef) {
+        // console.log("Document Written with ID: ", docRef.id);
+        resolve(console.log("Document Written with ID: ", docRef.id))
+      })
+      .catch(function (error) {
+        reject(console.error("Error Adding Document: ", error));
+      });
+  })
+}
+
 async function mainLoop() {
   var date = new Date();
   // console.log(date);
   console.log("Checking for new Tweet... " + "@" + ' ' + date)
   var data = await fetchPosts.fetchPosts();
-  // console.log(data);
+  var dataId = data[0].id;
+  var dataText = data[0].full_text;
 
-  var param;
-  // Iterate through data from fetchPosts
-  var dupe = await saveTweets.saveTweets(data[0]);
-  if (dupe === false) {
-    // console.log(dupe);
+  // console.log(dataId);
 
+  var storedTweets = await readDb();
+  // console.log(storedTweets);
+
+  var matches = false;
+  var needToWrite = false;
+
+  for (i = 0; i < storedTweets.length; i++) {
+    if (storedTweets[i].id === dataId) {
+      matches = true;
+      break;
+    }
+    //  else {
+    //   needToWrite = true;
+    // }
+  }
+
+  // console.log("Does it match? : " + matches);
+
+  if (matches == false) {
+    console.log("Writing Tweet to DB... " + "@" + ' ' + date);
+    await writeDb(dataId, dataText);
+    var param;
     param = data[0].full_text;
     // console.log(param);
 
     var memeUrlGenerated = await (fetchMemeUrl.fetchMemeUrl(spongebobify.spongebobify(param)));
+    // console.log(memeUrlGenerated);
     console.log("Generating Meme URL... " + "@" + ' ' + date);
     var encodedData = await imgToBase.imgToBase(memeUrlGenerated);
     console.log("Converting URL to base64..." + "@" + ' ' + date);
@@ -91,7 +182,7 @@ async function mainLoop() {
   }
 }
 
-setInterval(mainLoop, 10000);
+setInterval(mainLoop, 15000);
 
 //Mocking Trump 145802073
 //Spongebob 102156234
